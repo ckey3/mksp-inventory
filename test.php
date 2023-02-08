4f74d131f10656398b4d2d1dac0fa0b82422f276
ckey3 <coenkey3@gmail.com>
2023-02-08 14:29:34 -0600

    change

--- test.php
+++ test.php
@@ -4,7 +4,11 @@ echo"and you are too coen!!! ";
 
 echo $_GET;
 print_r($_GET);
+echo "Hello " . $_GET["name"];
 
-$f=fopen("test.json", "a");
-fwrite($f, "this is a file.");
+$f=fopen("test.json", "a");#open file
+fwrite($f, "this is a file.");#write something
+fclose($f);#close file
+echo "Saved";
+
 ?>
