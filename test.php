f2105c46c6fc5abaafb1668830fa17ac02665b8e
ckey3 <coenkey3@gmail.com>
2023-02-08 14:35:09 -0600

    change

--- test.php
+++ test.php
@@ -1,10 +1,14 @@
 <?php
 echo"Hi <b>Brooke</b>, you are late ";
-echo"and you are too coen!!! ";
+echo"and you are too coen!!!<br> ";
 
-echo $_GET;
+echo $_GET . "<br>";
 print_r($_GET);
 echo "Hello " . $_GET["name"];
+
+foreach ($_GET as $id){
+    echo $id . "==>" . $_GET[$id] . "<br>";
+}
 
 $f=fopen("test.json", "a");#open file
 fwrite($f, "this is a file.");#write something
