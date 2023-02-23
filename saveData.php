1788a113f57b3d1614658768e98010adc04754e9
ckey3 <coenkey3@gmail.com>
2023-02-23 13:41:43 -0600

    change

---
+++ saveData.php
@@ -0,0 +1,12 @@
+<?php
+
+//get POST data (json)
+$json = file_get_contents('php://input');
+//Converts it into PHP object
+$data = json_decode($json);
+
+//sends data back to the client that sent it
+echo json_encode($data);
+
+
+?>
